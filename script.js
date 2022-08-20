dc3689748e92982270f8d22f776421c2489c94f4
yojeongjin <osolku@naver.com>
2022-08-20 23:58:57 +0900

    Feat:strike & ball & playgame 기능구현
    -strike: 사용자가 입력한 숫자와 컴퓨터가 제공한 숫자의 자리수를 비교해주는 기능
    -ball: 사용자가 입력한 숫자와 컴퓨터가 제공한 숫자를 비교하여 자리수는 맞지않으나중복된 숫자가 있다면 중복된 수의 개수를 반환해주는 기능
    - playgame: 도전 button을 클릭하면 게임이 시작되게하는 기능 구현
    
    -

--- script.js
+++ script.js
@@ -1,57 +1,69 @@
-;(()=>{
+const $answer = document.querySelector('.answer_zone');
+const $input = document.querySelector('.input_zone');
+const $result = document.querySelector('.result_zone');
+const $btn = document.querySelector('.btn_zone');
 
-  const get = (target) => document.querySelector(target)
 
-  const init = () => {
-    get('form').addEventListner('submit', (event) => {
-      palyGame(event)
-    })
+const baseball = {
+  limit: 10,
+  digit: 4,
+  hit: 0,
+  end: false
+}
+
+let { limit, digit, hit, end } = baseball
+
+
+const randomNum = () => {
+  let num = []
+  let random = ''
+
+  for (let i=0; i<digit; i++) {
+    num[i] = parseInt(Math.random() * 10)
+    random += num[i]
   }
+  return random
+}
 
-  const basball = {
-    limit: 10,
-    digit: 4,
-    hit: 0,
-    end: false,
-    $answer: get('.answer_zone'),
-    $input: get('.input_zone'),
-    $result: get('.result_zone')
-  }
 
-  let { limit, digit, hit, end, $answer, $input, $result } = baseball
+const onHit = (inputNumber) => {
+  return `<span>${hit}차 </span> ${inputNumber}`
+}
 
-  // 컴퓨터가 제공하는 랜덤한 숫자
-  const randomNum = () => {
-    let num = []
-    let random = ''
+const getStrike = (inputNumber, random) => {
+  let strike = 0
+  const nums = inputNumber.split('')
 
-    for (let i=0; i<digit; i++) {
-      num[i] = parseInt(Math.random() * 10)
-      random += num[i]
+  nums.map((num, index)=>{
+    if(num === random[index]) {
+      strike++
     }
-  }
+  })
+  return strike
+}
 
-  // 시도했을 때
-  // const onHit = (사용자 입력 숫자) => {
-  //   return `<em>${hit}차</em> : {사용자 입력 숫자}`
-  // }
+const getBall = (inputNumber, random) => {
+  const nums = inputNumber.split('')
+  const rans = random.split('')
 
-  // strike
-  const strike = () => {}
+  const ballnum = nums.concat(rans)
 
-  //ball
-  const ball = () => {}
+  const findballs = ballnum => ballnum.filter((item, index) => ballnum.indexOf(item) !== index)
+  const ball = findballs.length
+  
+  return ball
+}
+
+
+$btn.addEventListener('click', ()=>{
+  const inputNumber = $input.value
+  const random = randomNum()
+  console.log(random)
 
   
-  const palyGame = (event) => {
-    event.preventDefault()
-
-    if(!!end) {
-      return
-    }
+  if (inputNumber !== random) {
+    hit ++
+    $result.innerHTML += `<br><span>${onHit(inputNumber)} STRIKE: ${getStrike(inputNumber, random)} BALL: ${getBall(inputNumber,random)}</span></br>`
   }
 
-
-
-  init()
-})()
+})
