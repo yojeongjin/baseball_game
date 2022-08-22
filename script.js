febf1ae87ca606a53ed833e88df7c957518f3682
yojeongjin <osolku@naver.com>
2022-08-22 20:37:47 +0900

    ball 기능 수정 및 alert 출력 fix: getBall 함수 오류 수정 feat: alert msg 창 출력 및 닫기 1. 사용자가 입력한 숫자와 컴퓨터가 제공하는 숫자의 자리수가 일치하였음에도 strike로 넘어가지않고 ball의 숫자가 count되는 현상 수정

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ const $input = document.querySelector('.input-zone');
 const $result = document.querySelector('.result-zone');
 const $btn = document.querySelector('.btn-zone');
 
-
 let limit = 10;
 let digit = 4;
 let hit = 0;
@@ -16,20 +15,37 @@ function gameStart() {
 
 function randomNum() {
   let num = []
-  let random = ''
+  let random = []
 
-  for (let i=0; i<digit; i++) {
+  for (let i=0; i<10; i++) {
     num[i] = parseInt(Math.random() * 10)
-    random += num[i]
+    if (random.indexOf(num[i]) === -1) {
+      random.push(num[i])
+    } 
   }
-
-  answer.push([random])
+  console.log(random)
+  answer.push(random.slice(0,4).join(''))
 }
 
 
 // 시도 횟수 및 사용자 입력숫자 알림
 function onHit(inputNumber) {
   return `<span>${hit}차 </span> ${inputNumber}`
+}
+
+// 중복 숫자 확인
+function isDuplicate(inputNumber) {
+  return [...new Set(inputNumber.split(''))].length !== digit
+}
+
+// alert 메시지 출력 
+const modalAlert = document.querySelector('.modal-alert');
+const titleAlert = document.querySelector('.modal-alert-title');
+
+function showAlert(alertMsg) {
+  titleAlert.innerHTML = `<span>📢 ${alertMsg}</span>`
+
+  modalAlert.classList.add('show')
 }
 
 // STRIKE
@@ -45,28 +61,56 @@ function getStrike(inputNumber, random) {
   return strike
 }
 
-// BALL
+//BALL
+
 function getBall(inputNumber, random) {
+  let ball = 0
   const nums = inputNumber.split('')
   const rans = random.split('')
 
-  const ball = 
-  nums.filter(item=>rans.includes(item))
-  
-  return ball.length
+  for (let i=0; i<digit; i++) {
+    if(random.indexOf(nums[i]) !== -1 && rans[i] !== nums[i]) {
+      ball++
+    }
+  }
+  return ball
 }
 
-$btn.addEventListener('click', () => {
+function initGame(e) {
+  e.preventDefault()
+
+  if(end === true) {
+    return
+  }
 
   const inputNumber = $input.value
   const random = answer[0].toString()
   console.log(random)
 
-  if (inputNumber !== random) {
+  if (inputNumber.length !== digit) { 
+    showAlert(`${digit}자리 숫자를 입력해주세요.<br></br>`)
+  } else if (isDuplicate(inputNumber)) {
+    showAlert('중복된 숫자가 있습니다.<br></br>')
+  } else  if(inputNumber !== random) {
     hit++
     $result.innerHTML += `<br><span>${onHit(inputNumber)} STRIKE: ${getStrike(inputNumber, random)} BALL: ${getBall(inputNumber,random)}</span></br>`
   }
-})
+}
+
+
+// alert창 닫기
+const modalCloseBtn = document.getElementById("modal-alert-close")
+
+modalAlert.addEventListener('click', (e) => {
+    if (e.target === modalAlert || e.target === modalCloseBtn) {
+        modalAlert.classList.remove('show')
+    }
+});
+
+
+
+
+$btn.addEventListener('click', initGame)
 
 window.onload = () => {
   gameStart()
