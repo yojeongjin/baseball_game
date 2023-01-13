406fcc04c131ff3a684979bd7d6f7280ac2a5540
yojeongjin <osolku@naver.com>
2023-01-14 02:09:58 +0900

    리팩토링 및 기능 추가 [feat] input을 네부분으로 나누어서 값 입력 시 자동으로 다음 필드로 이동하도록 구현하고 inputmode = numeric 을 추가하여 모바일에서는 숫자키패드가 출력되도록 구현함
    [refactor] 스타일 리팩토링
    
    -

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
 const $answer = document.querySelector('.answer-zone');
 const $notice = document.querySelector('.notice-zone');
-const $input = document.querySelector('.input-zone');
 const $result = document.querySelector('.result-zone');
-const $btn = document.querySelector('.btn-zone');
+const $btn = document.querySelector('.form__button');
+const $formInput = document.getElementsByClassName('form__input');
+const $trFirst = document.querySelector('.tablerow-first');
+const $trSecond = document.querySelector('.tablerow-second');
+const $trThird = document.querySelector('.tablerow-third');
+const $strike = document.getElementsByClassName('tablerow-second')[0];
+const $ball = document.getElementsByClassName('tablerow-third')[0];
 
-
-let limit = 10;
+let limit = 9;
 let digit = 4;
 let hit = 0;
 let end = false;
 let answer = [];
+let isModal = false;
 
 function gameStart() {
   randomNum()
 }
-
 
 function randomNum() {
   let num = []
@@ -26,14 +30,63 @@ function randomNum() {
       random.push(num[i])
     } 
   }
-  console.log(random)
   answer.push(random.slice(0,4).join(''))
 }
 
 
+const form = document.getElementsByClassName('input-wrapper')[0]
+const num1 = document.getElementById('num1')
+const num2 = document.getElementById('num2')
+const num3 = document.getElementById('num3')
+const num4 = document.getElementById('num4')
+
+form.addEventListener('keyup', function(e) {
+  if(end === true) {
+    return
+  }
+  
+  if (isModal === true) {
+    let timer = setTimeout(() => {
+      isModal = false
+    },10)
+  }
+
+  const regExp = /[^0-9]/g;
+
+  if (regExp.test(e.target.value)) {
+      e.target.value = "";
+  }
+
+  if(e.target.value.length === 1 && e.target.nextElementSibling) {
+    e.target.nextElementSibling.focus()
+  }
+
+  if(e.code === 'Enter' && isModal === false) {
+    checkInput()
+    console.log(hit, limit)
+  }
+})
+
+$btn.addEventListener('click', checkInput)
+
+function checkInput() {
+  const inputNumber = [num1.value, num2.value, num3.value, num4.value].join('')
+
+  if (inputNumber.length !== digit) { 
+    isModal = true;
+    return showAlert(`${digit}자리 숫자를 입력해주세요.`)
+  } else if (isDuplicate(inputNumber)) {
+    isModal = true;
+    return showAlert('중복된 숫자가 있습니다.')
+  } 
+
+  checkResult()
+}
+
 // 시도 횟수 및 사용자 입력숫자 알림
 function onHit(inputNumber,setRun) {
-  return `<span style="color:aqua">${hit}차</span><img src="./asstes/baseball-player.png" width="19px" height="19px"/> &emsp; ${inputNumber} &emsp; ${setRun}`
+  return `
+  <span>${hit}회</span> ${inputNumber} ${setRun}`
 }
 
 // 중복 숫자 확인
@@ -49,6 +102,11 @@ function showAlert(alertMsg) {
   titleAlert.innerHTML = `<span style="color: #33cc33">📢 ${alertMsg}</span>`
 
   modalAlert.classList.add('show')
+
+  for (let i=0; i < digit; i++) {
+    $formInput[i].value = ''
+  }
+  num1.focus()
 }
 
 // 유효성 확인 
@@ -98,40 +156,35 @@ function setRun(inputNumber, random) {
   const strike = getStrike(inputNumber, random)
   const ball = getBall(inputNumber, random)
 
-  return strike + '  '+ 'STRIKE' + '  '+ ball + '  '+ 'BALL'
+  return 'STRIKE' + '  ' + strike + '  '+ 'BALL'+ '  ' + ball 
 }
 
 
-
-
-function initGame(e) {
-  e.preventDefault()
+function checkResult() {
+  const inputNumber = [num1.value, num2.value, num3.value, num4.value].join('')
+  const random = answer[0].toString()
 
   if(end === true) {
     return
   }
+  hit++
+  const strike = getStrike(inputNumber, random)
+  const ball = getBall(inputNumber, random)
 
-  const inputNumber = $input.value
-  const random = answer[0].toString()
-  console.log(random)
-  isCorrect(inputNumber, random)
-  if (inputNumber.length !== digit) { 
-    showAlert(`${digit}자리 숫자를 입력해주세요.<br></br>`)
-  } else if (isDuplicate(inputNumber)) {
-    showAlert('중복된 숫자가 있습니다.<br></br>')
-  } else {
-    hit++
-    const output = onHit(inputNumber,setRun(inputNumber,random))
-    $result.innerHTML += `<br><span>${output}</span>`
-    
-    if (limit <= hit && !isCorrect(inputNumber, random)) {
-      showAlert('쓰리아웃!')
-      end = true
-    }
+  const output = onHit(inputNumber,setRun(inputNumber,random))
+  $result.innerHTML += `<span>${output}</span>`
+  $strike.getElementsByTagName("td")[`${hit}`].innerHTML = `<td>${strike}</td>`
+  $ball.getElementsByTagName("td")[`${hit}`].innerHTML = `<td>${ball}</td>` 
+
+  if (limit <= hit && !isCorrect(inputNumber, random)) {
+    end = true
+    return showAlert('쓰리아웃!')
   }
 
-  $input.value = ''
-  $input.focus()
+  for (let i=0; i < digit; i++) {
+    $formInput[i].value = ''
+  }
+  num1.focus()
 }
 
 // info-modal창 열기
@@ -153,7 +206,7 @@ modalAlert.addEventListener('click', (e) => {
 
 const infocCloseBtn = document.querySelector('.info-close');
 
-infocCloseBtn.addEventListener('click', (e) => {
+infocCloseBtn.addEventListener('click', () => {
   modalInfo.classList.remove('show')
 })
 
@@ -165,8 +218,25 @@ reStartBtn.addEventListener('click', () => {
 })
 
 
-$btn.addEventListener('click', initGame)
+function settingTr() {
+  for(let i=1; i<limit+1; i++) {
+    $trFirst.innerHTML +=  `<th scope="col">${i}</td>`
+  }
+  for(let i=1; i<limit+1; i++) {
+    $trSecond.innerHTML +=  `<td class="tabledata__data"></td>`
+  }
+  for(let i=1; i<limit+1; i++) {
+    $trThird.innerHTML += `<td class="tabledata__data"></td>`
+  }
+}
+
+document.addEventListener('keydown', (e) => {
+  if(e.code === 'Enter' && isModal === true) {
+    modalAlert.classList.remove('show')
+  }
+})
 
 window.onload = () => {
   gameStart()
+  settingTr()
 }
